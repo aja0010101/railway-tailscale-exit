a89a8803c4ed025d75b83988e0d0a7d91b691918
just for vpn <96411499+justforvpn@users.noreply.github.com>
2022-06-28 21:27:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /tmp/tailscale
 /var/runtime/tailscaled --tun=userspace-networking &
-until /var/runtime/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=railway-app --advertise-exit-node
+until /var/runtime/tailscaled up --authkey=${TAILSCALE_AUTHKEY} --hostname=railway-app --advertise-exit-node
 do
     sleep 0.1
 done
