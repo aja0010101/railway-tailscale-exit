b4dcb3bcffc3d0fb3942eb36c5a5310dcd399a5a
just for vpn <96411499+justforvpn@users.noreply.github.com>
2022-06-29 09:43:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/app/tailscaled --tun=userspace-networking & # --socks5-server=localhost:1055 &
+/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=railway-app --advertise-exit-node
 do
     sleep 0.1
