187bb66c62d49fdc835ef603bd349e86138a9926
just for vpn <96411499+justforvpn@users.noreply.github.com>
2022-07-01 14:53:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=railway-app --advertise-exit-node
-do
-    sleep 0.1
+until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=railway-app --advertise-exit-node; do
+  sleep 0.1
 done
+export ALL_PROXY=socks5://localhost:1055/
 sleep infinity 
 echo Tailscale started
